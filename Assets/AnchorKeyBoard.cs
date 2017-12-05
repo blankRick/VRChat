7edc899e4faa553fd95f3b57b6ce320194f1a505
blankRick <kinsukdas@ymail.com>
2017-12-05 14:30:16 +0530

    Fixed microphone detection in local machine

---
+++ Assets/AnchorKeyBoard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorKeyBoard : MonoBehaviour {
+    private Vector3 globalPos;
+    private Vector3 globalForward;
+    private bool assigned;
+    private void Start()
+    {
+        assigned = false;
+    }
+    
+    private void FixedUpdate()
+    {
+        if(assigned)
+        {
+            this.GetComponent<RectTransform>().position = globalPos;
+            this.GetComponent<RectTransform>().forward = globalForward;
+        }
+    }
+    public void anchor()
+    {
+        globalPos = this.GetComponent<RectTransform>().position;
+        globalForward= this.GetComponent<RectTransform>().forward;
+        assigned = true;
+    }
+    public void unfix()
+    {
+        assigned = false;
+    }
+}
--- Assets/RecordMicrophone.cs
+++ Assets/RecordMicrophone.cs
@@ -8,23 +8,30 @@ public class RecordMicrophone : NetworkBehaviour {
 
     // Use this for initialization
     AudioSource aud;
-	void Start () {
+    AudioListener listener;
+    GvrAudioListener gvrListener;
+    void Start () {
         if (!isLocalPlayer) return;
         aud = this.GetComponent<AudioSource>();
         if(Microphone.devices == null)
         {
-            GameObject.FindGameObjectWithTag("EditorOnly").GetComponent<Text>().text = "Not there";
+            GameObject.FindGameObjectWithTag("sendButton").GetComponent<Text>().text = "Not There";
             return;
         }
         else
         {
-            GameObject.FindGameObjectWithTag("EditorOnly").GetComponent<Text>().text = "there";
+            Debug.Log(Microphone.devices.Length);
+            GameObject.FindGameObjectWithTag("sendButton").GetComponent<Text>().text = "" + Microphone.devices[0];
         }
         aud.clip = Microphone.Start(Microphone.devices[0], true, 999, 44100);
         while (!(Microphone.GetPosition(Microphone.devices[0]) > 0))
         { }
         aud.Play();
-	}
+        listener = GetComponent<AudioListener>();
+        listener.enabled = true;
+        gvrListener = GetComponent<GvrAudioListener>();
+        gvrListener.enabled = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
